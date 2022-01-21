226fb54bf13c62361ab0b3d0022fbd7b4da58aa4
preetbhullar90 <gurpreet.singhb90@gmail.com>
2022-01-21 22:18:10 +0000

    Add content in admin file

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+#from ckeditor.fields import RichTextFieldModelAdmin
+from .models import Post, Comment
 
-# Register your models here.
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_on')
+    prepoulated_fileds = {'slug': ('title',)}
+    summernote_fields = ('content')
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
