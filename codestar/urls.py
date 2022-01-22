b9cd5ef4cf1b877b13cb700eaedf80ebd61818d5
preetbhullar90 <gurpreet.singhb90@gmail.com>
2022-01-22 00:55:41 +0000

    change file

--- codestar/urls.py
+++ codestar/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     #path('ckeditor/', include('ckeditor_uploader.urls')),
     #path('', index, name='index'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] #+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
