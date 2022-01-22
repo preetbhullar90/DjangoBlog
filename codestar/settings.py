6247c4583214ca3b50a610fe56d79cc92f02c0bc
preetbhullar90 <gurpreet.singhb90@gmail.com>
2022-01-22 00:52:20 +0000

    change media

--- codestar/settings.py
+++ codestar/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATIC_ROOT = os.path.join(BASE_DIR, 'media')#staticfiles
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')#staticfiles
 
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_stroage.storage.MediaCloudinaryStorage'
