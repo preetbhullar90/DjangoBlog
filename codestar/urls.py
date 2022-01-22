a6fe63d1e30f71234c156bb201ac4f2b5a69074b
preetbhullar90 <gurpreet.singhb90@gmail.com>
2022-01-22 00:53:59 +0000

    change directory

--- codestar/urls.py
+++ codestar/urls.py
@@ -23,6 +23,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ckeditor/', include('ckeditor_uploader.urls')),
+    #path('ckeditor/', include('ckeditor_uploader.urls')),
     #path('', index, name='index'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
