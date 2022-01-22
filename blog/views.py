17c439a29f879dbc0b65cf570d755cafd826f1aa
preetbhullar90 <gurpreet.singhb90@gmail.com>
2022-01-22 00:49:33 +0000

    change file

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Post
-def index(request):
-    blogs = Post.objects.all()
-    return render(request, 'blog/index.html', {'blogs': blogs})
+# from django.shortcuts import render
+# from .models import Post
+# def index(request):
+#     blogs = Post.objects.all()
+#     return render(request, 'blog/index.html', {'blogs': blogs})
--- codestar/urls.py
+++ codestar/urls.py
@@ -18,11 +18,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from blog.views import index
+#from blog.views import index
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
-    path('', index, name='index'),
+    #path('', index, name='index'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
