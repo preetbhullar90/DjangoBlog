fdea77d353aa1246562536409a1951eaf4287ac7
preetbhullar90 <gurpreet.singhb90@gmail.com>
2022-01-22 20:23:51 +0000

    heroku settings

--- codestar/settings.py
+++ codestar/settings.py
@@ -13,11 +13,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
-development = os.environ.get('DEVELOPMENTS', False)
-
 import dj_database_url
 if os.path.isfile("env.py"):
     import env
+
+development = os.environ.get('DEVELOPMENTS', False)
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -142,12 +143,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')#staticfiles
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
 
 MEDIA_URL = '/media/'
-#DEFAULT_FILE_STORAGE = 'cloudinary_stroage.storage.MediaCloudinaryStorage'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+DEFAULT_FILE_STORAGE = 'cloudinary_stroage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
