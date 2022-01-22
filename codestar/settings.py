dcdd7a69274ea6e7bf35a02e7120e78bd22d49c4
preetbhullar90 <gurpreet.singhb90@gmail.com>
2022-01-22 23:07:17 +0000

    test

--- codestar/settings.py
+++ codestar/settings.py
@@ -94,16 +94,16 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if development:
+#if development:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR / 'db.sqlite3'),
             }}
-else:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-        }
+#else:
+#    DATABASES = {
+#        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+#        }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
