778ae412609aedeac2827f35e5b644bbf795f781
preetbhullar90 <gurpreet.singhb90@gmail.com>
2022-01-23 16:20:43 +0000

    test

--- codestar/urls.py
+++ codestar/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('summernote/', include('django_summernote.urls')),
-    path('', include('blog.urls'), name='blog_urls'),
+    #path('summernote/', include('django_summernote.urls')),
+    #path('', include('blog.urls'), name='blog_urls'),
 ]
