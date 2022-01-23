06128b1ca2e19a0c0a8e805161364ecfef94e11f
preetbhullar90 <gurpreet.singhb90@gmail.com>
2022-01-23 16:29:38 +0000

    test

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 #from django_summernote.admin import SummernoteModelAdmin
 from .models import Post, Comment
-
+from django.conf import settings
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
